150f6c829ed90000ea67443f5964693301dff005
mavaldivie <marcos.valdivie@estudiantes.matcom.uh.cu>
2022-01-20 00:07:34 -0500

    Implemented Configuration module

--- Configuration.hs
+++ Configuration.hs
@@ -1,1 +1,22 @@
-var = value
+module Configuration where
+
+childs::Int
+childs = 4
+
+robots::Int
+robots = 2
+
+obstacles::Int
+obstacles = 20
+
+boardRows::Int 
+boardRows = 10
+
+boardColumns::Int 
+boardColumns = 10
+
+houseRows::Int 
+houseRows = 2
+
+houseColumns::Int 
+houseColumns = 2
