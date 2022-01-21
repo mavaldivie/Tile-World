0417894cdb191b55ba42d3e0d739911fbaba370a
mavaldivie <marcos.valdivie@estudiantes.matcom.uh.cu>
2022-01-21 00:09:19 -0500

    Fixed Main module

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Types
 import Init
 import ChildMovements
+import RobotMovements
 import Data.Array
 
 main::IO()
@@ -12,22 +13,26 @@ main= do
 
 loop::Board->IO()
 loop board = do 
-    putStr $ toString board ++ "\n"
-    x <- moveChilds board
-    loop x
-    -- if ended x
-    -- then putStr $ toString x ++ "\n"
-    -- else loop x
+    putStr $ toString board
+    let x = moveRobots board
+    putStr $ toString x
+    y <- moveChilds x
+
+    if ended y
+    then putStr $ toString y
+    else loop y
 
 ended::Board->Bool
 ended x = sum y == 0
             where 
                 n = rows x
                 m = cols x
+                fun p = x!p
                 y = [1 | i <- [0..n], j <- [0..m],
-                        x!(i,j) == Child|| 
-                        x!(i,j) == Muck ||
-                        x!(i,j) == ChildRobot]
+                        fun (i,j) == Child || 
+                        fun (i,j) == Muck ||
+                        fun (i,j) == ChildRobot ||
+                        fun (i,j) == MuckRobot]
 
 
 
