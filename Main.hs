5ecdc9210411f0531cc6b9e404fec197df5fda82
mavaldivie <marcos.valdivie@estudiantes.matcom.uh.cu>
2022-01-23 14:57:19 -0500

    Added information logs to main loop

--- Main.hs
+++ Main.hs
@@ -5,34 +5,53 @@ import Init
 import ChildMovements
 import RobotMovements
 import Data.Array
+import Utils
+import Configuration(shuffleInterval)
+
+
+import Debug.Trace(trace)
+logs::Show a=>String->a->a
+logs x y = trace (x ++ show y) y
+
 
 main::IO()
 main= do
     a <- initBoard
-    loop a
+    loop a 0 shuffleInterval
 
-loop::Board->IO()
-loop board = do 
+loop::Board->Int->Int->IO()
+loop board mx s = do 
     putStr $ toString board
-    let x = moveRobots board
+    let x = moveRobots board    
     putStr $ toString x
     y <- moveChilds x
 
-    if ended y
-    then putStr $ toString y
-    else loop y
+    let (n,m) = limits y
+        empty = length $ filter (\t -> y!t == Empty) [(i,j) | 
+                    i <- [0..n], j <- [0..m]]
+        muck = length $ filter (\t -> y!t == Muck) [(i,j) | 
+                    i <- [0..n], j <- [0..m]]
+        percent = muck * 100 `div` (empty + muck)
+        nmx = max mx percent
+    putStr $ "Muck percent: " ++ show percent ++ "%\n\n"
 
-ended::Board->Bool
-ended x = sum y == 0
-            where 
-                n = rows x
-                m = cols x
-                fun p = x!p
-                y = [1 | i <- [0..n], j <- [0..m],
-                        fun (i,j) == Child || 
-                        fun (i,j) == Muck ||
-                        fun (i,j) == ChildRobot ||
-                        fun (i,j) == MuckRobot]
+    if y == board && not (childMove y)
+    then putStr $ "Max muck percent: " ++ show nmx ++ "%\n" ++ "Finished\n"
+    else if s == 0
+        then do
+            putStr "Random Shuffle\n\n"
+            z <- shuffle y
+            loop z nmx shuffleInterval
+        else loop y nmx (s - 1)
+
+childMove::Board->Bool
+childMove b = any canMove childs
+    where 
+        (n,m) = limits b
+        childs = filter (\t -> b!t == Child) [(i,j) | 
+                    i <- [0..n], j <- [0..m]]
+        valid p d = first b (move p d) d /= (-1,-1)
+        canMove t = any (valid t) moves
 
 
 
